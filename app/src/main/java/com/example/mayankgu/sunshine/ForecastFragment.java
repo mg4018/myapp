dee6f7a1cd1ed7d1b62cff90807331ef778cc2df
mg4018 <mayankgu@MAYANKGU-LAP.oradev.oraclecorp.com>
2015-05-06 00:08:11 +0530

    Added Intent and detail activity

--- app/src/main/java/com/example/mayankgu/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/mayankgu/sunshine/ForecastFragment.java
@@ -1,4 +1,6 @@
 package com.example.mayankgu.sunshine;
+
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -103,6 +105,8 @@ public class ForecastFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String forecast = mForecastAdapter.getItem(position);
                 Toast.makeText(getActivity(), forecast,Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(),DetailActivity.class).putExtra(Intent.EXTRA_TEXT,forecast);
+                startActivity(intent);
 
             }
         });
